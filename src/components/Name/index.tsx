9704383ce2806363f85dc695a995f3e5c23d754a
hidetoshi93 <phunganhtuan.nd.vn@gmail.com>
2024-06-08 13:08:56 +0900

    update code

---
+++ src/components/Name/index.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import styles from "./index.module.scss";
+
+export const Name = () => {
+  const [name, setName] = useState("");
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const nameParam = urlParams.get("name");　//name={}
+    if (nameParam !== null) {
+      setName(nameParam);
+    }
+  }, [setName]);
+
+  return (
+    <div id="name" className={styles["name"]}>
+      {name}
+    </div>
+  );
+};
