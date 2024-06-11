0d3ec5a3a94048312b6ab1c4524da90007feb69e
hidetoshi93 <phunganhtuan.nd.vn@gmail.com>
2024-06-12 01:22:09 +0900

    update get picture from url param

--- src/components/Greetings/index.tsx
+++ src/components/Greetings/index.tsx
@@ -7,10 +7,18 @@ const Greetings = () => {
     greetings.items.length > 0
   );
   const [index, setIndex] = useState(0);
-
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const nameParam = urlParams.get("name");　//name={}
+  const nameImg = greetings.items[index].imageName;
+  const altText = nameParam ? `Picture of ${nameParam}` : 'Default picture';
+    
+  const imageUrl = nameParam && nameImg ? `/image/${nameParam}/${nameImg}` : '/image/default.png';
+  
   const handleNext = () => {
     if (index < greetings.items.length - 1) {
       setIndex(index + 1);
+
     } else {
       setShowGreetings(false);
     }
@@ -23,7 +31,8 @@ const Greetings = () => {
           <div className={styles["content"]}>
             <h3 className={styles["greetings-text"]}>{greetings.items[index].text}</h3>
             <img
-              src={greetings.items[index].image}
+              src={imageUrl}
+              alt={altText}
               className={styles["auto-fit-image"]}
             />
           </div>
---
+++ src/pages/index.tsx
@@ -0,0 +1,144 @@
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import { useCallback, useEffect, useRef, useState } from "react";
+import "@dotlottie/player-component";
+import "../styles/app.css";
+import { Cake } from "../components/Cake";
+import { Name } from "../components/Name";
+import Greetings from "../components/Greetings";
+import GiftBox from "../components/GiftBox";
+//import { useRouter } from 'next/router';
+
+const src = new URL("/assets/hbd2.mp3", import.meta.url).href;
+function App() {
+  const [candleVisible, setCandleVisible] = useState(true);
+
+  const audioRef = useRef<HTMLAudioElement>(new Audio(src));
+  const microphoneStreamRef = useRef<MediaStream | undefined>(undefined);
+
+  const blowCandles = useCallback(async (stream: MediaStream) => {
+    try {
+      microphoneStreamRef.current = stream;
+
+      const audioContext = new AudioContext();
+      const source = audioContext.createMediaStreamSource(stream);
+      const analyser = audioContext.createAnalyser();
+      source.connect(analyser);
+      analyser.fftSize = 2048;
+
+      const bufferLength = analyser.frequencyBinCount;
+      const dataArray = new Uint8Array(bufferLength);
+
+      const detectBlow = () => {
+        analyser.getByteFrequencyData(dataArray);
+        const average =
+          dataArray.reduce((acc, val) => acc + val, 0) / bufferLength;
+        const threshold = 43;
+
+        if (average > threshold) {
+          setCandleVisible(false);
+        }
+      };
+
+      setInterval(detectBlow, 100);
+    } catch (error) {
+      console.error("Error accessing microphone:", error);
+    }
+  }, []);
+
+  const onEnded = useCallback(() => {}, []);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+        });
+
+        if (stream) {
+          blowCandles(stream);
+        }
+      } catch (error) {
+        console.error("Error accessing microphone:", error);
+      }
+    })();
+
+    return () => {
+      if (microphoneStreamRef.current) {
+        microphoneStreamRef.current
+          .getTracks()
+          .forEach((track) => track.stop());
+      }
+    };
+  }, [blowCandles]);
+
+  const [showGiftBox, setShowGiftBox] = useState(true);
+  const handleGiftBoxClick = () => {
+    setShowGiftBox(false);
+    audioRef.current.load();
+    audioRef.current.play();
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100dvh",
+        justifyContent: "space-between",
+      }}
+    >
+      {showGiftBox && <GiftBox onClick={handleGiftBoxClick} />}
+      <Greetings />
+
+      <audio {...{ src, ref: audioRef, preload: "auto", onEnded }} />
+
+      <div>
+        <Name />
+        <Cake {...{ candleVisible }} />
+      </div>
+
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: "50%",
+          transform: "translateX(-50%)",
+        }}
+      >
+        <dotlottie-player
+          src="/assets/hbd.lottie"
+          autoplay
+          loop
+          style={{
+            zIndex: 20,
+            visibility: "visible",
+            width: 400,
+          }}
+        />
+      </div>
+
+      <div
+        style={{
+          position: "absolute",
+          top: "25%",
+          left: "50%",
+          transform: "translateX(-50%)",
+        }}
+      >
+        <dotlottie-player
+          src="/assets/confetti.lottie"
+          autoplay
+          loop
+          style={{
+            zIndex: 30,
+            visibility: "visible",
+            width: 400,
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
